e27280afac8028ede5d05740c673adf1841fcf4b
sv <sssssaaaaabbbbb@bk.ru>
2021-07-09 07:13:20 +0300

    1

--- itproger/itproger/urls.py
+++ itproger/itproger/urls.py
@@ -2,9 +2,11 @@
 отслеживаем различные URL-адреса  """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # из приложения main вызовем файл urls (его создаем) (для этого выше подключаем include)
     path('', include('main.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
